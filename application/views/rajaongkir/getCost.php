c5f33e7c6ff6eaaa3eae61eb13943479cb1b08ae
syabandz <syaban.mawardi@gmail.com>
2019-04-21 00:14:35 +0700

    api

--- application/views/rajaongkir/getCost.php
+++ application/views/rajaongkir/getCost.php
@@ -1,7 +1,6 @@
 <?php  
+
 $curl = curl_init();
-
-
 curl_setopt_array($curl, array(
   CURLOPT_URL => "http://api.rajaongkir.com/starter/cost",
   CURLOPT_RETURNTRANSFER => true,
@@ -21,56 +20,34 @@ $response = curl_exec($curl);
 $err = curl_error($curl);
 
 curl_close($curl);
-
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
-  //echo $response;
   $data = json_decode($response, true);
-  //echo json_encode($k['rajaongkir']['results']);
-
-  /*
-  for ($k=0; $k < count($data['rajaongkir']['results']); $k++){
-  
-
-    echo "<li='".$data['rajaongkir']['results'][$k]['code']."'>".$data['rajaongkir']['results'][$k]['service']."</li>";
-  	//echo $data['rajaongkir']['results'][$k]['code'];
-  }
-  */
-  //echo $data['rajaongkir']['results']['costs']['service'];
 }
 ?>
 
-<?php echo $data['rajaongkir']['origin_details']['city_name'];?> ke <?php echo $data['rajaongkir']['destination_details']['city_name'];?> @<?php echo $berat;?>gram Kurir : <?php echo strtoupper($courier); ?>
-
-<?php
- for ($k=0; $k < count($data['rajaongkir']['results']); $k++) {
-?>
-	 <div title="<?php echo strtoupper($data['rajaongkir']['results'][$k]['name']);?>" style="padding:10px">
-		 <table class="table table-striped">
-			 <tr>
-				 <th>No.</th>
-				 <th>Jenis Layanan</th>
-				 <th>ETD</th>
-				 <th>Tarif</th>
-			 </tr>
-			 <?php
-			 for ($l=0; $l < count($data['rajaongkir']['results'][$k]['costs']); $l++) {			 
-			 ?>
-			 <tr>
-				 <td><?php echo $l+1;?></td>
-				 <td>
-					 <div style="font:bold 16px Arial"><?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['service'];?></div>
-					 <div style="font:normal 11px Arial"><?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['description'];?></div>
-				 </td>
-				 <td align="center">&nbsp;<?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['etd'];?> days</td>
-				 <td align="right"><?php echo number_format($data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['value']);?></td>
-			 </tr>
-			 <?php
-			 }
-			 ?>
-		 </table>
-	 </div>
- <?php
- }
- ?>
+<?php echo "&nbsp;&nbsp;&nbsp;From ".$data['rajaongkir']['origin_details']['city_name'];?> To <?php echo $data['rajaongkir']['destination_details']['city_name'];?> @<?php echo $berat;?> gram Courier : <?php echo strtoupper($courier); ?>
+<?php for ($k=0; $k < count($data['rajaongkir']['results']); $k++) : ?>
+<div title="<?php echo strtoupper($data['rajaongkir']['results'][$k]['name']);?>" style="padding:10px">
+	<table class="table table-striped table-border table-hover">
+		<tr class="table-primary">
+			<th>No.</th>
+			<th>Service Type</th>
+			<th>ETD</th>
+			<th>Price</th>
+		</tr>
+		<?php for ($l=0; $l < count($data['rajaongkir']['results'][$k]['costs']); $l++): ?>
+		<tr>
+			<td><?php echo $l+1;?></td>
+			<td>
+				<div style="font:bold 16px Arial"><?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['service'];?></div>
+				<div style="font:normal 11px Arial"><?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['description'];?></div>
+			</td>
+			<td align="left">&nbsp;<?php echo $data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['etd'];?> days</td>
+			<td align="left"><?php echo number_format($data['rajaongkir']['results'][$k]['costs'][$l]['cost'][0]['value']);?></td>
+		</tr>
+	<?php endfor; ?>
+	</table>
+</div>
+<?php endfor; ?>
